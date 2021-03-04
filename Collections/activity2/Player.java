c349a5be0ecc2708364f9eba8f5686b6fa4dd86f
99003688 <78859930+99003688@users.noreply.github.com>
2021-03-04 20:55:13 +0530

    adding activities

---
+++ Collections/activity2/Player.java
@@ -0,0 +1,35 @@
+package com.ltts.Hash;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.TreeSet;
+public class Player{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in =new Scanner(System.in);
+	    HashSet<String> set=new HashSet();  
+
+	    int n;
+	    n=in.nextInt();
+	    String st;
+	    Scanner in1 =new Scanner(System.in);
+	    for(int i=0;i<n;i++) {
+	    	st=in1.nextLine();
+	    	set.add(st);
+	    }
+
+        TreeSet<String> treeSet = new TreeSet<String>(set); 
+	         Iterator<String> inn=treeSet.iterator();  
+	           while(inn.hasNext())  
+	           {  
+	            
+	            System.out.println(inn.next());
+	           }  
+	          
+	
+	}
+
+}
